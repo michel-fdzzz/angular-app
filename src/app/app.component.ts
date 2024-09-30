ad224b680c09f8996cfecb1447b7c5382e09e5b1
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-09-30 11:18:57 +0200

    iteracion array (ngFor), condicion (ngIf), etiquetar (ng-template)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], //El commmonModule lo he puesto para que funcione el ngFor
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  title = 'Hola mundo angular desde';//Si pones que sea pirvate el app.component.html da error
+  title = 'Hola mundo angular desde VSC';//Si pones que sea pirvate el app.component.html da error
   // Se puede especificar los tipos pero no hace falta
   // Explicacion age: number = 34
 
-  user: string[] = ['Pepe', 'María', 'Andrés']; //: string[] no es necesario
+  users: string[] = ['Pepe', 'María', 'Manuel'];  //: string[] no es necesario para especificar que es un array
+  //users: string[] = []; para comprobar si funci0one el *ngIf de la tabla al estar el array vacío 
+  /**
+   * ESTO INICIALIZA LA VARIABLE COMO SI NO TUVIERA ALGO O COMO SI DIERA IGUAL
+   *  users!: string[];
+   * users?: string[];
+   */
 }
